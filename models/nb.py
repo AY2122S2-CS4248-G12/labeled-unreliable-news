f2f4c29175c9a534646bbceaa1cbf5d74087d95b
zhaojj2209 <zhaojj2000@gmail.com>
2022-04-04 00:19:47 +0800

    Update NB code

--- models/nb.py
+++ models/nb.py
@@ -1,35 +1,32 @@
+import sys
 from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import make_pipeline
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
-import nltk
-nltk.download('wordnet')
-nltk.download('omw-1.4')
-from nltk import word_tokenize          
-from nltk.stem import WordNetLemmatizer 
 
-class LemmaTokenizer(object):
-    def __init__(self):
-        self.wnl = WordNetLemmatizer()
-    def __call__(self, articles):
-        return [self.wnl.lemmatize(t) for t in word_tokenize(articles)]
+sys.path.append("..")
+from preprocessing.preprocessor import Preprocessor
+
+preprocessor = Preprocessor(perform_case_folding=True,
+                            remove_stop_words=False,
+                            remove_punctuation=False,
+                            perform_lemmatization=False,
+                            perform_stemming=False)
 
 def main():
 
     # load data
-    train = pd.read_csv('fulltrain.csv', header=None)
-    test = pd.read_csv('balancedtest.csv', header=None)
+    train = pd.read_csv("../raw_data/fulltrain.csv", header=None)
+    test = pd.read_csv("../raw_data/balancedtest.csv", header=None)
     X_train = train[1]
     y_train = train[0]
     X_test = test[1]
     y_test = test[0]
     
     # Build the model
-    model = make_pipeline(CountVectorizer(analyzer='word',stop_words= 'english',lowercase=True, tokenizer = LemmaTokenizer()), MultinomialNB())
-    # model = make_pipeline(TfidfVectorizer(analyzer='word',stop_words= 'english',lowercase=True, tokenizer = LemmaTokenizer()), MultinomialNB())
+    model = make_pipeline(CountVectorizer(tokenizer=preprocessor.process), MultinomialNB())
     model.fit(X_train, y_train)
     predicted_categories = model.predict(X_test)
 
